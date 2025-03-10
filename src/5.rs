f8d7023c5f4cd73a8607ab25de0fb1311be14089
muschbrittania <muschbrittania@gmail.com>
2025-03-10 04:21:42 +0000

    Add 5.rs

---
+++ src/5.rs
@@ -0,0 +1,9 @@
+
+use std::collections::HashMap;
+
+fn main() {
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Alice"), 10);
+    scores.insert(String::from("Bob"), 20);
+    scores.insert(String::from("Charlie"), 30);
+}
