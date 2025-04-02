01016279012999869de3b5a3675f45af22ac761b
muschbrittania <muschbrittania@gmail.com>
2025-04-02 15:20:14 +0000

    Add 22.rs

---
+++ src/22.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 5;
+    if x > 10 {
+        println!("x is greater than 10");
+    } else if x == 10 {
+        println!("x is exactly 10");
+    } else {
+        println!("x is less than 10");
+    }
+}
